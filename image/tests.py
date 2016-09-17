92848d206dc7e1463b83d835b6d6c6295c5a30f6
Mike <mikekh@uw.edu>
2016-09-17 10:43:29 -0700

    commit current changes.

--- image/tests.py
+++ image/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.contrib.auth import User
+from .models import Photo    
 
 # Create your tests here.
+
+class PhotoTestCase(TestCase):
+    """Test case for photo Model"""
+
+    def setUp(self):
+        """Setup photo model."""
+        self.user = User(username='Cris', first_name='Cris')
+        Photo(
+            user=self.user, 
+            title='image1',
+            description='The first photo.',
+        ).save()
