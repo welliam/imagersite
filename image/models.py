873d04fa9531487aa189ac0b5ac63c648e1bf2ab
Mike <mikekh@uw.edu>
2016-09-18 14:46:33 -0700

    Make a migration.

--- image/models.py
+++ image/models.py
@@ -1,8 +1,7 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils.encoding import python2_unicode_compatible
-
+from django.utils.encoding import python_2_unicode_compatible
 # Create your models here.
 
 def photo_path(instance, filename):
@@ -13,7 +12,9 @@ def photo_path(instance, filename):
 class Photo(models.Model):
     """Information about photo."""
     photo = models.ImageField(
-        upload_to = 'albums',
+        upload_to = photo_path,
         blank=True,
         null=True
     )
+    title = models.CharField(max_length=128)
+    description = models.TextField()
