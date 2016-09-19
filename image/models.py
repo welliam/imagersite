10bfe6d00edc6e77a5e64590522c7e3eebe6a3a6
Mike <mikekh@uw.edu>
2016-09-18 19:36:42 -0700

    Added Photo model. Can't well. Tests wont' pass. value error can't save.

--- image/models.py
+++ image/models.py
@@ -4,6 +4,13 @@ from django.contrib.auth.models import User
 from django.utils.encoding import python_2_unicode_compatible
 # Create your models here.
 
+
+PUB_CHOICES = (
+    ('Public', 'Public'),
+    ('Private', 'Private'),
+    ('Shared', 'Shared'),
+)
+
 def photo_path(instance, filename):
     """Create file path for the photo."""
     return "{0}/{1}".format(instance, filename)
@@ -11,6 +18,12 @@ def photo_path(instance, filename):
 @python_2_unicode_compatible
 class Photo(models.Model):
     """Information about photo."""
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.deletion.CASCADE,
+        related_name='photos',
+        null=True,
+    )
     photo = models.ImageField(
         upload_to = photo_path,
         blank=True,
@@ -18,3 +31,11 @@ class Photo(models.Model):
     )
     title = models.CharField(max_length=128)
     description = models.TextField()
+    date_uploaded = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    date_published = models.DateTimeField(auto_now_add=True)
+    pusblished = models.CharField(
+        max_length=7,
+        choices=PUB_CHOICES,
+        default='Public'
+    )
--- image/tests.py
+++ image/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 from .models import Photo    
+from datetime import datetime
 
 # Create your tests here.
 
@@ -12,7 +13,7 @@ class PhotoTestCase(TestCase):
         self.user = User(username='Cris', first_name='Cris')
         Photo(
             user=self.user, 
-            title='image1',
+            title='image1.jpg',
             description='The first photo.'
         ).save()
 
@@ -25,3 +26,11 @@ class PhotoTestCase(TestCase):
         """Test photo has a description."""
         self.assertEqual(self.user.photo.description, 'The first photo.')
     
+    def test_photo_path(self):
+        """Test the path of the file."""
+        self.assertIn('/media/image1.jpg', self.user.photo.upload_to)
+
+    def test_date_uploaded(self):
+        """Test uploaded date."""
+        date = datetime.utcnow().strftime("%Y-%m-%d")
+        self.assertEqual(self.user.photo.date_uploaded, date)
