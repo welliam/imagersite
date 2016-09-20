efbbd50e41807eb8d050a2ab149c3f8e982852b7
Justin Lange <well1912@gmail.com>
2016-09-19 18:18:42 -0700

    Start implementation registration/ templates

--- imagersite/urls.py
+++ imagersite/urls.py
@@ -13,16 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
 
-from .views import home_view, register_view, login_view
+from .views import home_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home_view),
-    url(r'^register$', register_view),
-    url(r'^login$', login_view)
+    url(r'^accounts/', include('registration.backends.hmac.urls'))
 ]
