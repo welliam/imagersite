2f6bd08872cc4a5c9009962aaeedd99ba2b4d16f
Justin Lange <well1912@gmail.com>
2016-09-22 10:12:02 -0700

    Remove unused views

--- imagersite/views.py
+++ imagersite/views.py
@@ -4,13 +4,3 @@ from django.shortcuts import render
 def home_view(request):
     """Return rendered home page."""
     return render(request, 'home.html', context={})
-
-
-def register_view(request):
-    """Return rendered register page."""
-    return render(request, 'register.html', context={})
-
-
-def login_view(request):
-    """Return rendered login page."""
-    return render(request, 'login.html', context={})
