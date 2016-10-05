352176ef8418bfecf1a86cb31aff6183d15ac4cf
mikekh84 <mikekh@uw.edu>
2016-10-05 15:12:34 -0700

    Fixed import error typo.

--- imager_api/views.py
+++ imager_api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def photo_api_view(request, format=None):
+    """Get photo api"""
+    return Response({})
