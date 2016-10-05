84ac183f37e0c2aadb315db162a247ef0c824cee
mikekh84 <mikekh@uw.edu>
2016-10-05 14:43:56 -0700

    Setup for PhotoAPI tests.

--- imager_api/tests.py
+++ imager_api/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from factory.django import DjangoModelFactory, ImageField
 
-# Create your tests here.
+
+class PhotoFactory(DjangoModelFactory):
+    class Meta(object):
+        model = Photo
+    photo = ImageField()
+
+
+class PhotoApiTestCase(TestCase):
+    """Test case for Photo Api."""
+
+    def setUp(self):
+        """Setup for photo api testcase."""
+        self.user = User(username='Bob')
+        self.user.save()
+
+        self.client.force_login(self.user)
+        self.photo = PhotoFactory(
+            user=self.user,
+            title='A title',
+        )
+        self.photo.save()
+
