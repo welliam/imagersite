4ac1509e7a49d2baa7d0c237ba4a52539e74530a
mikekh84 <mikekh@uw.edu>
2016-10-05 15:39:25 -0700

    Updated views to include serialzier and return data.

--- imager_api/views.py
+++ imager_api/views.py
@@ -1,8 +1,13 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from imager_api.serializer import PhotoSerializer
+from image.models import Photo
+
 
 @api_view(['GET'])
 def photo_api_view(request, format=None):
     """Get photo api"""
-    return Response({})
+    photos = Photo.objects.all()
+    serializer = PhotoSerializer(photos, many=True)
+    return Response(serializer.data)
