9282c2ea467bdd63a5ecd138516fdcea2b39c98e
mikekh84 <mikekh@uw.edu>
2016-10-20 19:32:42 -0700

    fixed for ansible.

--- imagersite/settings.py
+++ imagersite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bqlvqs3$%rg&c=+tn@m&$l73vm=3(stkl$-4j$1m%3r3!t(8hd'
+SECRET_KEY = os.environ['FL_SECURITY_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -132,6 +132,7 @@ USE_TZ = True
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
